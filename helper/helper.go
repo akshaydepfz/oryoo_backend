ad919d12653a32a6acab19ed3f1cedbd51b3cfa6
Akshay <akshaypk..dev@gmail.com>
2025-11-26 19:11:21 +0530

    master

--- helper/helper.go
+++ helper/helper.go
@@ -5,14 +5,3 @@ import (
 )
 
 var DB *sql.DB
-
-func InsertCustomer(name, email string) (uint, error) {
-	var id uint
-	query := `INSERT INTO customers (name, email, created_date) 
-              VALUES ($1, $2, NOW()) RETURNING id`
-	err := DB.QueryRow(query, name, email).Scan(&id)
-	if err != nil {
-		return 0, err
-	}
-	return id, nil
-}
