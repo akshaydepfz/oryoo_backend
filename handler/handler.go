e7d0f65043ef60b9b021d434e297345a70f0f88d
Akshay <akshaypk..dev@gmail.com>
2025-12-26 21:52:57 +0530

    master

--- handler/handler.go
+++ handler/handler.go
@@ -492,7 +492,7 @@ func GetLatestVersionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	version := models.VersionResponse{
-		Version:     "1.0.7",
+		Version:     "1.2.0",
 		UpdateURL:   "https://play.google.com/store/apps/details?id=com.oryoo.app",
 		IsMandatory: true,
 	}
